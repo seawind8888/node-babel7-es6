9548b513ff2ab017a12ab14a644b98be640fcd2b
seawind8888 <344023858@qq.com>
2019-07-15 13:57:21 +0800

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+class App {
+    constructor(){}
+    test = () => {
+        console.log('test')
+    }
+}
+export default App
+
+const _app = new App()
+_app.test()
---
+++ index.js
@@ -0,0 +1,2 @@
+require('@babel/register');
+require('./app') // 要运行的文件
